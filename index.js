864d6a5b712c45b4ae6f0803f700e43fb046540e
Daniel Koss <danielkoss@Daniels-MacBook-Air.local>
2020-09-20 14:43:27 -0400

    Reading twitter bearer token

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-exports.run = (req, res) => {
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+const name = process.env.TWITTER_API_BEARER_SECRET_LOC;
+
+exports.run = async (req, res) => {
   res.send('we ran!');
+  const client = new SecretManagerServiceClient();
+
+  const [version] = await client.accessSecretVersion({
+    name: name,
+  });
+
+  // Extract the payload as a string.
+  const payload = version.payload.data.toString();
+
+  // WARNING: Do not print the secret in a production environment - this
+  // snippet is showing how to access the secret material.
+  console.info(`Payload: ${typeof payload}`);
 };
